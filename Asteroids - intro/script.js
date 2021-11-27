9f6c2787bce6cbd78a23b68b3673910ca598f4ec
Inês Reis <40200335@esmad.ipp.pt>
2021-11-27 00:18:20 +0000

    Animação e ajustes feitos

---
+++ Asteroids - intro/script.js
@@ -0,0 +1,84 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+let background = new Image();
+background.src = "img/fundo.png";
+
+let width = canvas.width
+let height = canvas.height
+let startMenu = document.querySelector("#startMenu")
+let startButton = document.querySelector("#startButton")
+
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+// window.addEventListener("resize", function() {
+//     canvas.width = window.innerWidth
+//     canvas.height = window.innerHeight
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     drawBackground()
+// })
+
+
+setTimeout(function () {
+    location.href = '../Asteroids-Game-main/index.html';
+}, 7700);
+
+
+background.onload = function () {
+    ctx.drawImage(background, 0, 0)
+}
+
+startButton.onclick = function () {
+    fadeOut(startMenu)
+}
+
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    ctx.drawImage(background, 0, 0)
+    // drawBackground()
+})
+
+
+
+
+
+
+
+//Functions
+
+
+
+
+
+
+function fadeOut(element) {
+
+    var op = 1; // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+            start()
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50)
+
+
+}
+
+
+function start() {
+
+    //Variables
+    ctx.beginPath()
+    ctx.arc(100, 100, 10, 0, Math.PI * 2, true)
+    ctx.fillStyle = "white"
+    ctx.fill()
+}
