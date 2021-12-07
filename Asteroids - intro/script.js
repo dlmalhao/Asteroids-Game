a5976f4455bc36a56a9c37e4e610102dbf113941
Inês Reis <40200335@esmad.ipp.pt>
2021-12-07 01:12:49 +0000

    Formatações, ajustes e comentários adicionados (parte 1)

--- Asteroids - intro/script.js
+++ Asteroids - intro/script.js
@@ -1,47 +1,40 @@
+/* Elementos necessários para que o fundo seja responsivo */
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 let background = new Image();
 background.src = "img/fundo.png";
 
-let width = canvas.width
-let height = canvas.height
-let startMenu = document.querySelector("#startMenu")
-let startButton = document.querySelector("#startButton")
+
+/* Canvas height e width */
+let width = canvas.width;
+let height = canvas.height;
 
 
-
-/* Sounds */
-
+/* Sons e a música */
 let music = document.getElementById("initialMusic");
-const bullet = document.getElementById("gunshotSound")
-const explosion = document.getElementById("explosionSound")
-const asteroidHover = document.getElementById("asteroidSound")
-const ufoSound = document.getElementById("ufoSound")
+const bullet = document.getElementById("gunshotSound");
+const explosion = document.getElementById("explosionSound");
+const asteroidHover = document.getElementById("asteroidSound");
+const ufoSound = document.getElementById("ufoSound");
 
-window.focus()
+/* window.focus(); */
 
-
-/* Adjust background */
-
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
+/* Ajustar e desenhar o fundo  no canvas*/
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 background.onload = function () {
-    ctx.drawImage(background, 0, 0)
-}
-
+    ctx.drawImage(background, 0, 0);
+};
 
 window.addEventListener("resize", function () {
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    ctx.drawImage(background, 0, 0)
+    ctx.drawImage(background, 0, 0);
     // drawBackground()
-})
+});
 
-
-/* Sound player */
-
+/* Tocador de som de acordo com as animações*/
 setTimeout(function () {
     asteroidHover.play();
     setTimeout(function () {
@@ -51,15 +44,11 @@ setTimeout(function () {
             setTimeout(function () {
                 explosionSound.play();
             }, 300);
-        }, 2000)
+        }, 2000);
     }, 5000);
-}, 0)
+}, 0);
 
-
-
-/* Load the new page transition */
+/* Tiro dispara, nave sobre e vai para a próxima página assim que acaba a animação */
 setTimeout(function () {
-    location.href = '../Asteroids-Game-main/index.html';
+    location.href = "../Asteroids-Game-main/index.html";
 }, 7700);
-
-
